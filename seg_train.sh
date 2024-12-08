0c290c69a62cda81833089544c1395906831605d
Ziang <47993354+EricXuziang@users.noreply.github.com>
2024-12-08 19:36:21 +0000

    Delete seg_train.sh

--- seg_train.sh
+++
@@ -1,9 +0,0 @@
-# for fold in {0..5}
-# do 
-#     # echo "nnUNetv2_train 1 3d_lowres $fold"
-#     nnUNetv2_train 001 3d_fullres $fold -p nnUNetResEncUNetMPlans
-# done
-
-nnUNetv2_train 001 3d_fullres 0 -p nnUNetResEncUNetMPlans
-
-# nnUNetv2_predict -i /well/rittscher/users/ycr745/nnUNet/nnunetv2/dataset/nnUNet_raw/Dataset001_quiz/imagesVal -o /well/rittscher/users/ycr745/nnUNet/nnunetv2/results/val -c 3d_fullres -d 001 -f 0
